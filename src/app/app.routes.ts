ddec1abce6a4de5c752204f222161bdf4cdd3b4f
Uros Jovanov <ujovanov@gmail.com>
2026-01-22 14:58:18 +0100

    Added initial components for cart and registration and creating login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Singletoy } from './singletoy/singletoy';
+import { Registration } from './registration/registration';
+import { Login } from './login/login';
+import { Profile } from './profile/profile';
 
 export const routes: Routes = [
   {
@@ -10,5 +13,21 @@ export const routes: Routes = [
   {
     path: 'igracke/:permalink',
     component: Singletoy
+  },
+  {
+    path: 'registration',
+    component: Registration
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'profile',
+    component: Profile
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
